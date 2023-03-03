cbc1d7ba55766ee7613cc66f1078f31dc7056455
Roque Martinez <roquemn5@gmail.com>
2023-03-02 19:59:52 -0800

    week 4 assignment, let and typeof

---
+++ 007-variable/scripts/main.js
@@ -0,0 +1,3 @@
+let fname = "Roque";
+const myHeading = document.querySelector("h1");
+myHeading.textContent = fname;
---
+++ 008-let-and-typeof/scripts/main.js
@@ -0,0 +1,8 @@
+let food = 'pizza'
+console.log('food is pizza which is a typeof', (typeof food))
+
+let slices = 8
+console.log('slices is the quantity which is type of', (typeof slices))
+
+let tasty = true
+console.log('tasty is the flavor which is a type of', (typeof tasty))
