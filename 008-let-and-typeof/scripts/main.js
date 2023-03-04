9a08d2efd713e57105d7f88d3a1e02247354a2ee
Roque Martinez <roquemn5@gmail.com>
2023-03-03 20:05:43 -0800

    more on variables

--- 008-let-and-typeof/scripts/main.js
+++ 008-let-and-typeof/scripts/main.js
@@ -1,8 +1,26 @@
-let food = 'pizza'
-console.log('food is pizza which is a typeof', (typeof food))
-
+let food = '8'
 let slices = 8
-console.log('slices is the quantity which is type of', (typeof slices))
-
 let tasty = true
-console.log('tasty is the flavor which is a type of', (typeof tasty))
+let newVar = 'SOMETHING'
+const myConst = 42
+console.log(slices == food)
+console.log(slices === food)
+
+let myVar = null
+console.log(`myVar is typeof ${typeof myVar}`)
+
+console.log(`food is typeof ${typeof food}`)
+console.log(`food is value ${food}`)
+console.log(`slices is typeof ${typeof slices}`)
+console.log(`tasty is typeof ${typeof tasty}`)
+
+//  DON'T USE VAR
+// USE CONST, ITS A BIT MORE STABLE
+
+// if (true) {
+//     const myConst = 43
+//     console.log(myConst)
+// } else {
+
+// }
+
