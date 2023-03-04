c6fb6f06be3ac2df2de1003bfcba711d9bf05290
Roque Martinez <roquemn5@gmail.com>
2023-03-03 20:47:45 -0800

    fun with functions

---
+++ 009-functions/main.js
@@ -0,0 +1,10 @@
+// functions are fun
+
+function doMath (a, b) {
+    console.log(`the result is ${a - b}`)
+    console.log(`the result is ${a + b}`)
+    console.log(`the result is ${a / b}`)
+    console.log(`the result is ${a * b}`)
+}
+
+doMath(10, 4)
