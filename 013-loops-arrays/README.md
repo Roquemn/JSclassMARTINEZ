5c28ff309ac7eba4b9dd1a77261acb664a5d531b
Roque Martinez <roquemn5@gmail.com>
2023-04-05 20:01:12 -0700

    my code for cal average cost

---
+++ 013-loops-arrays/README.md
@@ -0,0 +1,8 @@
+# Week 7 Loops and Arrays
+
+---
+
+### How am I improving my use of devTools
+
+I have started to pay more attention to error lines more. It's such a time saver being able to pinpoint syntax or other errors shown by devTool. Sometimes the simplest of errors can take a long time to see, really glad to have this tool at my disposal!!
+
--- 013-loops-arrays/main.js
+++ 013-loops-arrays/main.js
@@ -10,7 +10,7 @@ const updateDOM = (input) => {
     divEl.appendChild(p)
 }
 
-const trackMPGCost = (miles, gallons, price = 3.79) => {
+const trackMPGandCost = (miles, gallons, price = 3.79) => {
     const MPG = Math.round(miles/gallons)
     const tripCost = Math.round(gallons * price)
     updateDOM(`Miles per gallon is ${MPG} and trip cost is ${tripCost}`)
@@ -20,22 +20,22 @@ const trackMPGCost = (miles, gallons, price = 3.79) => {
 }
 
 const calculateMPGAvg = () => {
-    // const avgMPG = (myArr[0] + myArr[2]) / 2
-    // updateDOM(`Average Mile per gallon is ${avgMPG}`)
     let totalMPG = 0
     for(let i = 0; i < myMPG.length; i++) {
         totalMPG = totalMPG + myMPG[i]
     }
-    let avgMPG = totalMPG/myMPG.length
+    let avgMPG = Math.round(totalMPG/myMPG.length)
     updateDOM(`Average MPG is ${avgMPG}`)
 }
 
-// calculate average we need to determine total of occurrences and divide that number of occurrences
+//calculateAvgCost
+// commit message "my code for cal average cost"
+// * round average MPG 
 
-trackMPGCost(360, 15, 5.40)
-trackMPGCost(320, 12, 5)
-trackMPGCost(100, 7, 4.40)
-trackMPGCost(600, 24, 5.70)
-trackMPGCost(50, 2, 3)
-trackMPGCost(320, 12, 5)
+trackMPGandCost(360, 15, 5.40)
+trackMPGandCost(320, 12, 5)
+trackMPGandCost(100, 7, 4.40)
+trackMPGandCost(600, 24, 5.70)
+trackMPGandCost(50, 2, 3)
+trackMPGandCost(320, 12, 5)
 calculateMPGAvg()
