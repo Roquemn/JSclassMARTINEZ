2346e6bfc306991d5471fe2ada54e63094a76eb3
Roque Martinez <roquemn5@gmail.com>
2023-04-09 16:01:00 -0700

    working form

--- 015-user-input/main.js
+++ 015-user-input/main.js
@@ -38,7 +38,11 @@ const calculateAvg = () => {
 }
 
 FORM.addEventListener('submit', (e) => {
-    console.log(e)
+    e.preventDefault()
+    const miles = parseInt(e.target.miles.value)
+    const gallons = parseInt(e.target.gallons.value)
+    const price = parseInt(e.target.price.value)
+    trackMPGandCost(miles, gallons, price)
 })
 
 // trackMPGandCost(360, 15, 5.40)
