21354489e470ed1fd305cad0af100af589755ab9
Roque Martinez <roquemn5@gmail.com>
2023-04-11 18:53:49 -0700

    Solution Code

--- 016-form-validation/main.js
+++ 016-form-validation/main.js
@@ -1,11 +1,12 @@
 const FORM = document.getElementById("form-input");
 const ERR = document.getElementById("err");
+const AVG_OUTPUT = document.getElementById("output-avg");
 
 const MY_MPG = [];
 const MY_TRIP_COST = [];
 
-const updateDOM = (input) => {
-  const divEl = document.querySelector("#output");
+const updateDOM = (input, id) => {
+  const divEl = document.querySelector(id);
   const p = document.createElement("p");
   p.textContent = input;
   divEl.appendChild(p);
@@ -14,7 +15,10 @@ const updateDOM = (input) => {
 const trackMPGandCost = (miles, gallons, price = 3.79) => {
   const MPG = Math.round(miles / gallons);
   const tripCost = Math.round(gallons * price);
-  updateDOM(`Miles per gallon is ${MPG} and trip cost is ${tripCost}`);
+  updateDOM(
+    `Miles per gallon is ${MPG} and trip cost is ${tripCost}`,
+    "#output"
+  );
   MY_MPG.push(MPG);
   MY_TRIP_COST.push(tripCost);
 };
@@ -32,8 +36,8 @@ const calculateAvg = () => {
   let sumTripCost = calculateSUM(MY_TRIP_COST);
   let avgMPG = Math.round(sumMPG / MY_MPG.length);
   let avgTripCost = Math.round(sumTripCost / MY_TRIP_COST.length);
-  updateDOM(`Average MPG is ${avgMPG}`);
-  updateDOM(`Average Trip is ${avgTripCost}`);
+  updateDOM(`Average MPG is ${avgMPG}`, "#output-avg");
+  updateDOM(`Average Trip Cost is ${avgTripCost}`, "#output-avg");
 };
 
 FORM.addEventListener("submit", (e) => {
@@ -44,15 +48,17 @@ FORM.addEventListener("submit", (e) => {
   const price = parseInt(e.target.price.value);
   if (miles === 0 || gallons === 0 || price === 0) {
     errMsg.push("Make sure you input value greater than 0!! Try Again");
-  } 
-
+  }
   if (price > 1000) {
-    errMsg.push('Really!!!?? I think this is in error...Try again')
+    errMsg.push("Really!!!?? I think this is in error...Try again");
   }
-
   if (errMsg.length > 0) {
-    ERR.textContent = errMsg
-} else {
+    ERR.textContent = errMsg;
+  } else {
+    ERR.textContent = "";
+    AVG_OUTPUT.textContent = "";
     trackMPGandCost(miles, gallons, price);
+    calculateAvg();
   }
+  FORM.reset();
 });
