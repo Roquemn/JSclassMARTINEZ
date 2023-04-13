d56e61045a5d382895c4f05c1ae1919efcac5aec
Roque Martinez <roquemn5@gmail.com>
2023-04-12 17:45:54 -0700

    the this

--- 017-intro-to-objects/main.js
+++ 017-intro-to-objects/main.js
@@ -5,17 +5,18 @@ const myArr = [];
 // console.log(myArr === myObj)
 const myNumber = 9;
 const myObj = {
-  name: "roque",
+  name: "Roque",
   age: 36,
-  hobbies: ["running", "sleeping", "reading"],
+  hobbies: ["running", "sleep", "reading"],
   mobile: {
     make: "Samsung S21",
     OS: "android",
   },
   myGreeting: function (person) {
-    return `What is up ${person}?`;
+    console.log(this)
+    return `${this.name} who loves to ${this.hobbies[1]} says What is up ${person}?`;
   },
 };
 
-const greeting = myObj.myGreeting('Roque')
+const greeting = myObj.myGreeting('Erica')
 console.log(greeting)
