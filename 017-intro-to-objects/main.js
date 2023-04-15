81c111bc3d5024ef0401cfcf157fce4be2b91a04
Roque Martinez <roquemn5@gmail.com>
2023-04-15 13:37:26 -0700

    += objects

--- 017-intro-to-objects/main.js
+++ 017-intro-to-objects/main.js
@@ -7,16 +7,31 @@ const myNumber = 9;
 const myObj = {
   name: "Roque",
   age: 36,
+  likes: 0,
   hobbies: ["running", "sleep", "reading"],
   mobile: {
     make: "Samsung S21",
     OS: "android",
   },
   myGreeting: function (person) {
-    console.log(this)
+    console.log(this);
     return `${this.name} who loves to ${this.hobbies[1]} says What is up ${person}?`;
+  },
+  increaseLikes: function () {
+    this.likes += 1;
   },
 };
 
-const greeting = myObj.myGreeting('Erica')
-console.log(greeting)
+const roqueObj = myObj;
+roqueObj.increaseLikes();
+console.log(roqueObj);
+roqueObj.age = 37;
+
+const greeting = myObj.myGreeting("Erica");
+console.log(greeting);
+
+console.log(myObj.mobile.make.length);
+
+for (key in myObj) {
+  console.log(`key ${key} and value ${myObj[key]}`);
+}
