0587279612365d5cc78834dec9b304b493c58087
Roque Martinez <roquemn5@gmail.com>
2023-05-03 20:48:12 -0700

    working code

--- 027-intro-to-opp/main.js
+++ 027-intro-to-opp/main.js
@@ -5,42 +5,46 @@ class Person {
     this._address = address;
   }
   getFirstName() {
-    return `${this._firstName}`
+    return `${this._firstName}`;
   }
   setFirstName(newFirstName) {
-    this._firstName = newFirstName
-  }
-  showStatus() {
-    console.log(`${this.getFirstName} lives at ${this.address}`);
+    this._firstName = newFirstName;
   }
   getLastName() {
-    return `${this._lastName}`
+    return `${this._lastName}`;
   }
   setLastName(newLastName) {
-    this._lastName = newLastName
-  }
-  showStatus() {
-    console.log(`${this.getFirstName} lives at ${this.address}`);
+    this._lastName = newLastName;
   }
   getAddress() {
-    return `${this._address}`
+    return `${this._address}`;
   }
   setAddress(newAddress) {
-    this._address = newAddress
+    this._address = newAddress;
   }
   showStatus() {
-    console.log(`${this.getFirstName} lives at ${this.address}`);
+    console.log(`${this.getFirstName()} lives at ${this.getAddress()}`);
   }
 }
 
 class Student extends Person {
   constructor(firstName, lastName, address, classList) {
     super(firstName, lastName, address);
-    this.classList = classList;
+    this._classList = classList;
   }
-  showStatus() {
-    console.log(`${this.firstName} lives at ${this.address} is taking ${this.classList}`);
+  getClassList() {
+    return `${this._classList}`;
+  }
+  setClassList(newClassList) {
+    this._classList = newClassList;
+  }
 
+
+
+  showStatus() {
+    console.log(
+      `${this.getFirstName()} lives at ${this.getAddress()} is taking ${this.getClassList()}`
+    );
   }
 }
 
@@ -52,5 +56,4 @@ roque.showStatus();
 const jane = new Person("Jane", "Doe", "444 Short Way Drive");
 jane.showStatus();
 
-console.log(roque.getFirstName())
-
+console.log(roque.getFirstName());
