44f8f7d6b86ba038117548f4671edf0f303dc716
Roque Martinez <roquemn5@gmail.com>
2023-05-04 19:18:35 -0700

    intro to promises

---
+++ 031-promises/main.js
@@ -0,0 +1,38 @@
+function getWeather() {
+  return new Promise(function (resolve, reject) {
+    setTimeout(() => {
+      resolve("Rainy");
+    }, 100);
+  });
+}
+
+function getWeatherIcon(weather) {
+  return new Promise(function (resolve, reject) {
+    setTimeout(() => {
+      switch (weather) {
+        case "Sunny":
+          resolve("☀️");
+          break;
+        case "Cloudy":
+          resolve("☁️");
+          break;
+        case "Rainy":
+          resolve("🌧️");
+          break;
+        default:
+          reject("NO ICON FOUND");
+      }
+      resolve("Sunny");
+    }, 100);
+  });
+}
+
+function onSuccess(data) {
+  console.log(`Success ${data}`);
+}
+
+function onError(error) {
+  console.log(`Error: ${error}`);
+}
+
+getWeather().then(getWeatherIcon).then(onSuccess, onError);
