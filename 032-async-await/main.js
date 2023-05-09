798d564d2e95e953f948af5b48d309d19f43af14
Roque Martinez <roquemn5@gmail.com>
2023-05-08 19:25:36 -0700

    try/catch

--- 032-async-await/main.js
+++ 032-async-await/main.js
@@ -1,17 +1,37 @@
+// async function start() {
+//   const data = await fetch(
+//     "https://api.weather.gov/gridpoints/HNX/53,100/forecast"
+//   );
+//   const result = await data.json();
+//   console.log(result.properties.periods[1].shortForecast);
+// }
+
+// async function start2() {
+//   fetch("https://api.weather.gov/gridpoints/HNX/53,100/forecast")
+//     .then((data) => data.json())
+//     .then((result) => {
+//       console.log(result.properties.periods[1].shortForecast);
+//     });
+// }
+
+// start2();
+
+function getData() {
+  return new Promise(function (resolve, reject) {
+    setTimeout(() => {
+      //   resolve("HERE IS YOUR DATA");
+      reject("Something went wrong");
+    }, 1);
+  });
+}
+
 async function start() {
-  const data = await fetch(
-    "https://api.weather.gov/gridpoints/HNX/53,100/forecast"
-  );
-  const result = await data.json();
-  console.log(result.properties.periods[1].shortForecast);
+  try {
+    const result = await getData();
+    // SUCCESS
+  } catch (error) {
+    // FAILURE
+  }
 }
 
-async function start2() {
-  fetch("https://api.weather.gov/gridpoints/HNX/53,100/forecast")
-    .then((data) => data.json())
-    .then((result) => {
-      console.log(result.properties.periods[1].shortForecast);
-    });
-}
-
-start2();
+start();
