431a249767d34316553b39396774fbd79ada8c0f
Roque Martinez <roquemn5@gmail.com>
2023-05-10 19:51:02 -0700

    API keys

--- 034-fetch-api/main.js
+++ 034-fetch-api/main.js
@@ -1,4 +1,5 @@
-const url = "http://worldtimeapi.org/api/timezone/America/Los_Angeles";
+const url =
+  "https://api.thenewsapi.com/v1/news/top";
 
 async function getData() {
   const response = await fetch(url);
