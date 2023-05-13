740179afd8cb87ff3b6b6caa5f2c2c0ec35d71ba
Roque Martinez <roquemn5@gmail.com>
2023-05-12 20:41:33 -0700

    finish basic types

--- 036-typescript/src/main.ts
+++ 036-typescript/src/main.ts
@@ -7,4 +7,13 @@ let album: any;
 myName = "martinez";
 meaningOfLife = 42
 isLoading = true
-album = 1984
+album = 5150
+
+const sum = (a: number, b: string) => {
+    return a + b
+}
+
+let postId: string | number
+let isActive: number | boolean
+
+let re: RegExp = /\w+/g
