2d1f04760b86e3da09676c82c6566950a2ba5117
Roque Martinez <roquemn5@gmail.com>
2023-05-12 21:12:50 -0700

    object type

--- 036-typescript/build/js/main.js
+++ 036-typescript/build/js/main.js
@@ -9,3 +9,18 @@ guitars.unshift('Jim');
 let test = [];
 let bands = [];
 bands.push('Van Halen');
+//Tuple
+let myTuple = ['roque', 36, true];
+let mixed = ['martinez', 1, false];
+myTuple[1] = 42;
+// Objects
+let myObj;
+myObj = [];
+console.log(typeof myObj);
+myObj = bands;
+myObj = {};
+const exampleObj = {
+    prop1: "Roque",
+    prop2: true,
+};
+exampleObj.prop1 = 'roque';
--- 036-typescript/src/main.ts
+++ 036-typescript/src/main.ts
@@ -13,3 +13,42 @@ guitars.unshift('Jim')
 let test = []
 let bands: string[] = []
 bands.push('Van Halen')
+
+//Tuple
+let myTuple: [string, number, boolean] = ['roque', 36, true]
+
+let mixed = ['martinez', 1, false]
+
+myTuple[1] = 42
+
+// Objects
+let myObj: object
+myObj = [];
+console.log(typeof myObj);
+myObj = bands;
+myObj = {};
+
+const exampleObj = {
+  prop1: "Roque",
+  prop2: true,
+};
+
+exampleObj.prop1 = 'roque';
+
+type Guitarist = {
+    name: string,
+    active: boolean,
+    albums: (string | number) []
+}
+
+let evh: Guitarist = {
+    name: 'Eddie',
+    active: false,
+    albums: [1984, 5150, 'OU812']
+}
+let jp: Guitarist = {
+    name: 'Jimmy',
+    active: true,
+    albums: ['I', 'II', 'IV']
+}
+
